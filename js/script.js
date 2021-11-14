0b5dbd20e362cab6c540a1e5064d961f257d1e02
jeroen13 <jeroenkemna@hotmail.com>
2021-11-14 14:51:32 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -331,6 +331,8 @@ async function makeRequest(method, url) {
     return new Promise(function (resolve, reject) {
         let xhr = new XMLHttpRequest();
         xhr.open(method, url);
+        xhr.responseType = 'arraybuffer';
+        
         xhr.onload = function () {
             if (this.status >= 200 && this.status < 300) {
                 resolve(xhr.response);
